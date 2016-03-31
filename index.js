19338edecf575d82dba7145f55186c087d8371b9
rajayogan <rajayogan27@gmail.com>
2016-04-01 04:30:02 +0530

    initial commit

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ var io = require('socket.io'),
 
 io.on('connection', function(socket) {
     console.log('User Connected');
-    socket.on('message', function(msg){
-        io.emit('message', msg);
+    socket.on('message', function(datamsg){
+        io.emit(datamsg.room+'message', datamsg.data);
     });
+    socket.on('userentered', function(user){
+        io.emit(user.room+'userentered', user.data);
+    });
+    socket.on('userleave', function(user){
+        io.emit(user.room+'userleave', user.data);
+    });
+    
 });
 
 server.listen(3000, function(){
